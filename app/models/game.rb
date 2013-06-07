2a5a5908a10881432d464523be84ed80ee47474e
acacheung <acacheung@gmail.com>
2013-06-07 01:49:52 -0400

    added format time method for game created times

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,9 @@
 class Game < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :created_at
 
   validates_presence_of :name
+
+  def format_time(created_at)
+    created_at.strftime("%b%e %l%P")
+  end
 end
