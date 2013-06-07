b2d9910f590915774696a8ef0437d51eeda9869c
acacheung <acacheung@gmail.com>
2013-06-07 11:45:58 -0400

    addedgame player tests

---
+++ spec/features/games_have_game_players_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe 'adding game players' do
+  it 'can see the game players that belong to each game on game index' do
+    visit root_path
+    click_link 'Track a new game'
+    fill_in 'Name', :with => 'Mahjong1'
+    fill_in 'Player 1', :with => 'Amanda'
+    fill_in 'Player 2', :with => 'Sam'
+    fill_in 'Player 3', :with => 'Matt'
+    click_button 'Done'
+    page.should have_content('Amanda')
+  end
+
+  it 'can see the game players on each game show page' do
+    visit root_path
+    click_link 'Track a new game'
+    fill_in 'Name', :with => 'Mahjong1'
+    fill_in 'Player 1', :with => 'Amanda'
+    fill_in 'Player 2', :with => 'Sam'
+    fill_in 'Player 3', :with => 'Matt'
+    click_button 'Done'
+    click_button 'Mahjong1'
+    page.should have_content('Amanda')
+  end
+end
