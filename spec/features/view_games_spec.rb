9be7768040859da17877f0d7df664e19f3a09a08
acacheung <acacheung@gmail.com>
2013-06-07 01:17:09 -0400

    reversed order of games so most recent is on top

--- spec/features/view_games_spec.rb
+++ spec/features/view_games_spec.rb
@@ -8,4 +8,15 @@ describe 'add games' do
     click_button 'Done'
     page.should have_content 'Mahjong'
   end
+
+  it 'more recent games are at the top' do
+    visit root_path
+    click_link 'Track a new game'
+    fill_in 'Name', :with => 'Mahjong1'
+    click_button 'Done'
+    click_link 'Track a new game'
+    fill_in 'Name', :with => 'Mahjong2'
+    click_button 'Done'
+    page.should have_content(/Mahjong2.*Mahjong1/)
+  end
 end
