d8a447eda6d8f37555b1fa4df0113c53e94fef5d
acacheung <acacheung@gmail.com>
2013-06-06 23:40:36 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BreakableScorekeeper::Application.config.secret_token = '8e1c330f7ebda8acfc8dd92881fa2d4b5dc3fbb940b7006a7691bc769a2dd0540fb0a3cb97b486ef8348412f11921db0c2a14bb6f8454ca0f5598b722e61a28b'
