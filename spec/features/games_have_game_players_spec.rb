26fcfbfb914708c2e86ebf5e55fb69b6e7b06ab8
acacheung <acacheung@gmail.com>
2013-06-10 17:10:04 -0400

    refactor games index

--- spec/features/games_have_game_players_spec.rb
+++ spec/features/games_have_game_players_spec.rb
@@ -5,32 +5,28 @@ describe 'adding game players' do
   it 'asks for player names when tracking a new game' do
     visit root_path
     click_link 'Track a new game'
-    fill_in 'What is the name of your game?', :with => 'Mahjong'
-    fill_in 'How many players?', :with => '4'
-    click_button 'Enter'
-    page.should have_content(/Player.*{4}/)
+    page.should have_content(/Player.1/)
   end
 
   it 'can see the game players that belong to each game on game index'
   #   visit root_path
   #   click_link 'Track a new game'
-  #   fill_in 'Name', :with => 'Mahjong1'
+  #   fill_in 'What is the name of your game?', :with => 'Mahjong1'
   #   fill_in 'Player 1', :with => 'Amanda'
   #   fill_in 'Player 2', :with => 'Sam'
   #   fill_in 'Player 3', :with => 'Matt'
-  #   click_button 'Done'
+  #   click_button 'Enter'
   #   page.should have_content('Amanda')
   # end
 
-  it 'can see the game players on each game show page'
-  #   visit root_path
-  #   click_link 'Track a new game'
-  #   fill_in 'Name', :with => 'Mahjong1'
-  #   fill_in 'Player 1', :with => 'Amanda'
-  #   fill_in 'Player 2', :with => 'Sam'
-  #   fill_in 'Player 3', :with => 'Matt'
-  #   click_button 'Done'
-  #   click_button 'Mahjong1'
-  #   page.should have_content('Amanda')
-  # end
+  it 'can see the game players on each game show page' do
+    visit root_path
+    click_link 'Track a new game'
+    fill_in 'What is the name of your game?', :with => 'Mahjong1'
+    fill_in 'Player 1', :with => 'Amanda'
+    fill_in 'Player 2', :with => 'Sam'
+    fill_in 'Player 3', :with => 'Matt'
+    click_button 'Enter'
+    page.should have_content('Amanda')
+  end
 end
